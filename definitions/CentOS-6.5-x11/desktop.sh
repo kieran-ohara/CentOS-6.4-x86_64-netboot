3e6d87642d8c4c926191e546a7f553ffaed20375
Kieran Bamforth <hello@kieranbamforth.me>
2014-05-15 20:22:50 +0100

    Gave up with the desktop shortcuts. Ah well.

--- definitions/CentOS-6.5-x11/desktop.sh
+++ definitions/CentOS-6.5-x11/desktop.sh
@@ -3,15 +3,3 @@ sed -i 's/id:3/id:5/' /etc/inittab
 sed -i '4iAutomaticLoginEnable=true\nAutomaticLogin=root' /etc/gdm/custom.conf
 
 yum -y install vim-X11
-
-if ln -s /etc/gdm/custom.conf /root/Desktop; then
-        echo 'Created custom.conf symlink on desktop.'
-else
-        echo 'Failed to create custom.conf symlink on desktop'
-fi 2>/dev/null
-
-if ln -s /etc/inittab /root/Desktop; then
-        echo 'Created inittab symlink on desktop.'
-else
-        echo 'Failed to create inittab symlink on desktop'
-fi 2>/dev/null
