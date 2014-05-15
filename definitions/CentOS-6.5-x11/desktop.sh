dd4139db14e24e65e6cdde2f6040cf52eeca9fb8
Kieran Bamforth <hello@kieranbamforth.me>
2014-05-15 08:05:20 +0100

    Renamed sensitives to .dist files.

--- definitions/CentOS-6.5-x11/desktop.sh
+++ definitions/CentOS-6.5-x11/desktop.sh
@@ -4,6 +4,14 @@ sed -i '4iAutomaticLoginEnable=true\nAutomaticLogin=root' /etc/gdm/custom.conf
 
 yum -y install vim-X11
 
-#ln -s /etc/init.d /root/Desktop
-ln -s /etc/gdm/custom.conf /root/Desktop
-ln -s /etc/inittab /root/Desktop
+if ln -s /etc/gdm/custom.conf /root/Desktop; then
+        echo 'Created custom.conf symlink on desktop.'
+else
+        echo 'Failed to create custom.conf symlink on desktop'
+fi 2>/dev/null
+
+if ln -s /etc/inittab /root/Desktop; then
+        echo 'Created inittab symlink on desktop.'
+else
+        echo 'Failed to create inittab symlink on desktop'
+fi 2>/dev/null
