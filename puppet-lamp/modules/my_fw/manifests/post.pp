77e8d6d1f30ab5e50b56ebe7347f9d8b1b8a9424
Kieran Bamforth <kieranb@studioskylab.com>
2014-06-08 23:34:35 +0100

    Stopped firewall from blocking access.

--- puppet-lamp/modules/my_fw/manifests/post.pp
+++ puppet-lamp/modules/my_fw/manifests/post.pp
@@ -1,7 +1,7 @@
 class my_fw::post {
-  firewall { '999 drop all':
-    proto   => 'all',
-    action  => 'drop',
-    before  => undef,
-  }
+#  firewall { '999 drop all':
+#    proto   => 'all',
+#    action  => 'drop',
+#    before  => undef,
+#  }
 }
